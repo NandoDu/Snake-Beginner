3fc0929769c274158ad74e31eb9939c932d54e6d
NandoDu <181250028@smail.nju.edu.cn>
2021-11-08 16:03:07 +0800

    update

---
+++ Assets/Scripts/SnakeHead.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class SnakeHead : MonoBehaviour
+{
+    public int step;  // one move of snake
+    private int x;  // change in the x axis
+    private int y;  // change in the y axis
+    private Vector3 headPos;  // the position of snake now
+
+    void Move() {
+        
+    }
+}
